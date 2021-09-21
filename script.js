add524ef41b4a935e23ed8d16008dd6d84e40e19
“Rajul” <rajulkoshta10@gmail.com>
2021-09-21 11:45:01 +0530

    added changes

--- script.js
+++ script.js
@@ -4,38 +4,38 @@ const currentPrice = document.querySelector("#current-price");
 const tellMeBtn = document.querySelector("#tell-me-btn");
 const message = document.querySelector("#message-output");
 
- tellMeBtn.addEventListener("click",btnHandler);
-  
- 
- function calculateProfitOrLoss(initial,current,quantity){
-    if(current > initial ){
-        var profit = (current-initial)*quantity;
-        var profitPercentage = (profit/initial)*100;
+tellMeBtn.addEventListener("click", btnHandler);
+
+
+function calculateProfitOrLoss(initial, current, quantity) {
+    if (current > initial) {
+        var profit = (current - initial) * quantity;
+        var profitPercentage = (profit / initial) * 100;
         message.style.color = "green";
         showmessage(`Yay! 🤑🤑🥳🥳your profit is ${profit} and profit percentage ${profitPercentage.toFixed(2)}%`);
-    }
-    else if(initial > current){
-        var loss = (initial-current)*quantity;
-        var lossPercentage = (loss/initial)*100;
+    } else if (initial > current) {
+        var loss = (initial - current) * quantity;
+        var lossPercentage = (loss / initial) * 100;
         message.style.color = "red";
         showmessage(`uffff! 😔😌😩😩your loss is ${loss} and loss percentage ${lossPercentage.toFixed(2)}%`);
-    
-    }
-    else{
+
+    } else {
         showmessage(`No pain no gain , no gain no pain 🐱‍🏍🐱‍🏍!!`);
     }
 }
 
- function btnHandler(){
-     var initial = Number(initialPrice.value);
-     var current = Number(currentPrice.value);
-     var quantity = Number(quantityOfStocks.value);
+function btnHandler() {
+    var initial = Number(initialPrice.value);
+    var current = Number(currentPrice.value);
+    var quantity = Number(quantityOfStocks.value);
+    if (initial <= 0 || current <= 0 || quantity <= 0) {
+        showmessage("negative values not allowed");
+    } else {
+        var profitOrLoss = calculateProfitOrLoss(initial, current, quantity);
+    }
+}
 
-     var profitOrLoss = calculateProfitOrLoss(initial,current,quantity);
 
- }
-
-
- function showmessage(msg){
-     message.innerText = msg;      
- }
+function showmessage(msg) {
+    message.innerText = msg;
+}
